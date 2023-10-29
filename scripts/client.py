079e662c194f38f4485db536dac167a4d0e67cea
Orkun Acar <111832118+Orkun00@users.noreply.github.com>
2023-10-29 15:57:00 +0300

    Update client.py

--- scripts/client.py
+++ scripts/client.py
@@ -1,13 +1,13 @@
 import socket
 import threading
 
-# Choose a nickname for the client
+# Choosing a nickname for the client
 nickname = input("Choose your nickname: ")
 
-# Create the client socket
+# Creating the client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the client socket to the server
+# Connecting the client socket to the server
 client.connect(('127.0.0.1', 8765))
 
 # Function to handle receiving messages from the server
@@ -31,10 +31,10 @@ def write():
         message = '{}: {}'.format(nickname, input(''))
         client.send(message.encode('ascii'))
 
-# Create and start the receiving thread
+# After this create and start the receiving thread
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-# Create and start the writing thread
+# After this create and start the writing thread
 write_thread = threading.Thread(target=write)
 write_thread.start()
